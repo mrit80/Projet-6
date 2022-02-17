d6bb0d2d07203d4e4c63f6325a2c8309b06455c3
Mikael Romeo <mikaelro@live.fr>
2022-02-17 19:51:26 +0100

    Mikael project

---
+++ Projet 6/backend/routes/sauce.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
+const sauceCtrl = require('../controllers/sauce');
+
+router.get('/', sauceCtrl.getAllSauce);
+router.post('/', auth, multer, sauceCtrl.createSauce);
+router.get('/:id', sauceCtrl.getOneSauce);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
+router.delete('/:id', sauceCtrl.deleteSauce);
+router.post('/:id/like', auth, sauceCtrl.setLikeDislike);
+
+module.exports = router;
